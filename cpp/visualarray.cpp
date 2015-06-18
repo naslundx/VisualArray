173e54cec70524b34ad10de8ab0ac1faba93b993
naslundx <naslundx@gmail.com>
2015-06-18 21:53:51 +0200

    Improved, not yet functional

--- cpp/visualarray.cpp
+++ cpp/visualarray.cpp
@@ -1,7 +1,6 @@
 /* TODO
  *
  * So many things
- * Missing: Window and graphics management
  */
 
 template<class T>
@@ -26,43 +25,139 @@ T VisualArray::operator[](int i) const {
 
 template<class T>
 T& VisualArray::operator [](const int i) {
-	// Will this allow me to notice change?
+	// This will not allow me to notice change :(
+	// Figure out how to do this without specific set function
+
+	// Add change to mHistory:
+	std::vector<int> op;
+	op.push_back(HISTORY_TYPE.SET);
+
+	//...
+
+	mHistory.push_back(op);
 }
 
-void VisualArray::gfxHighlight(index, Color) {
+template<class T>
+void VisualArray::gfxHighlight(int index, Color) {
+	if (index >= 0 && index < mSize) {
+		std::vector<int> op;
+		op.push_back(HISTORY_TYPE.HIGHLIGHT);
+		op.push_back(index);
+
+		// handle color
+
+		mHistory.push_back(op);
+	}
+}
+
+template<class T>
+void VisualArray::gfxColor(int index, Color) {
+	if (index >= 0 && index < mSize) {
+		std::vector<int> op;
+		op.push_back(HISTORY_TYPE.COLOR);
+		op.push_back(index);
+
+		// handle color
+
+		mHistory.push_back(op);
+	}
+}
+
+template<class T>
+void VisualArray::gfxDecolor(int index) {
+	if (index >= 0 && index < mSize) {
+		std::vector<int> op;
+		op.push_back(HISTORY_TYPE.DECOLOR);
+		op.push_back(index);
+
+		// handle color
+
+		mHistory.push_back(op);
+	}
+}
+
+template<class T>
+void VisualArray::gfxSeparate(int leftindex) {
+	if (leftindex >= -1 && index <= mSize) {
+		std::vector<int> op;
+		op.push_back(HISTORY_TYPE.SEPARATE);
+		op.push_back(index);
+
+		// handle color
+
+		mHistory.push_back(op);
+	}
 
 }
 
-void VisualArray::gfxColor(index, Color) {
+template<class T>
+void VisualArray::gfxDeseparate(int leftindex) {
+	if (leftindex >= -1 && index <= mSize) {
+		std::vector<int> op;
+		op.push_back(HISTORY_TYPE.DESEPARATE);
+		op.push_back(index);
+
+		// handle color
+
+		mHistory.push_back(op);
+	}
 
 }
 
-void VisualArray::gfxDecolor(index) {
-
-}
-
-void VisualArray::gfxSeparate(index) {
-
-}
-
+template<class T>
 void VisualArray::hideLastOperation() {
 	if (!mHistory.empty()) {
 		mHistory.pop_back();
 	}
 }
 
+template<class T>
 void VisualArray::clearHistory() {
 	mHistory.clear();
+	for (int i=0; i<mSize; i++) {
+		mOriginal[i] = mData[i];
+	}
 }
 
+template<class T>
 void VisualArray::render() {
+	// Init rendering window, create rectangles
+	mRectangles.clear();
+	mLastHighlights.clear();
 
+	// ... 
+
+	// Start rendering loop
+	// ...
 }
 
+template<class T>
 std::vector<int> VisualArray::nextOperation() {
+	std::vector<int> op = mHistory.pop_front();
 
+	if (op[0] == HISTORY_TYPE.SET) {
+		if (!mHistory.empty()) {
+			std::vector<int> opb = mHistory.pop_front();
+
+			// Check if swap
+			if (...) {
+				op[0] = HISTORY_TYPE.SWAP;
+				op[2] = opb[1];
+				return op;
+			}
+			else {
+				mHistory.push_front(opb);
+			}
+		}
+	}
+
+	return op;
 }
 
+template<class T>
 void VisualArray::renderNext() {
-
+	std::vector<int>::iterator it;
+	for (it = mLastHighlights.begin(); it != mLastHighlights.end(); ++it) {
+		// Set rectangle to default color
+	}
 }
--- cpp/visualarray.h
+++ cpp/visualarray.h
@@ -10,22 +10,27 @@ public:
 	unsigned T operator [](int i) const;
 	unsigned T& operator [](const int i);
 
-	void gfxHighlight(index, Color);
-	void gfxColor(index, Color);
-	void gfxDecolor(index);
-	void gfxSeparate(index);
+	void gfxHighlight(int index, Color);
+	void gfxColor(int index, Color);
+	void gfxDecolor(int index);
+	void gfxSeparate(int leftindex);
+	void gfxDeseparate(int leftindex);
 	void hideLastOperation();
 	void clearHistory();
 	void render();
 
 private:
+	static enum HISTORY_TYPE { SET, SWAP, COLOR, DECOLOR, HIGHLIGHT, SEPARATE, DESEPARATE };
+
 	std::vector<int> nextOperation();
 	void renderNext();
 
 	int mSize;
 	T *mData;
 	T *mOriginal;
-	std::deque<std::vector<int>> mHistory;
+	std::deque< std::vector<int> > mHistory;
 	std::vector<int> mRectangles;
 	std::vector<int> mLastHighlights;
+
+	// Missing: default color, highlight color
 };
