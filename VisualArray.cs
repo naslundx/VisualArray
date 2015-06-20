45d143cbae548e1dacf13c7ca07552fff68ab8b9
Marcus Näslund <naslundx@gmail.com>
2015-06-20 21:35:10 +0200

    More stuff, nothing really works yet

--- VisualArray.cs
+++ VisualArray.cs
@@ -1,23 +1,35 @@
 //Note: Adapted from the C++ version. Nothing really works yet.
 
-//TODO: Add extra class/struct which handles the history events
-
 //TODO: Template this class so that it works with all kinds of comparable members
-
 public class VisualArray
 {
+    public class VisualArrayHistory
+    {
+	public VisualArray.HISTORY_TYPE type;
+	public int pos;
+	public int data;
+	public float width;
+	public Color color;
+    }
+    
     private static enum HISTORY_TYPE { SET, SWAP, COLOR, DECOLOR, HIGHLIGHT, SEPARATE, DESEPARATE };
-    private Color gfxDefaultColor, gfxDefaultHighlightColor, gfxDefaultSeparatorColor;
+    
     private int[] mData, mOriginal;
-	  private int mSize; // is this necessary?
+    
+    List<VisualArrayHistory> mHistory;
+    List<int> mRectangles;
+    List<int> mLastHighlights;
 
-	  //std::deque< VisualArrayHistory > mHistory;
-	  //std::vector<int> mRectangles;
-	  //std::vector<int> mLastHighlights; 
-	  
-	  //TODO: get/set default colors
+    private Color gfxDefaultColor, gfxDefaultHighlightColor, gfxDefaultSeparatorColor;
+    //TODO: get/set default colors
 	  
 	  //TODO: add whatever operators that are standard on C# arrays
+
+    public VisualArray(int size)
+    {
+    	mData = new int[size];
+    	mOriginal = new int[size];
+    }
 
     public VisualArray(int[] array) 
     {
@@ -46,21 +58,39 @@ public class VisualArray
       }
     }
     
+    public IEnumerator<int> GetEnumerator() 
+    { 
+    	return mData.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    { 
+    	return GetEnumerator();
+    }
+    
     //TODO: Handle get reference to elements?
     
 	  public void gfxHighlight(int index, Color color)
 	  {
 	    if (index >= 0 && index < mData.Length)
 	    {
-	    
+	    	VisualArrayHistory _event;
+	    	_event.type = HISTORY_TYPE.HIGHLIGHT;
+	    	_event.pos = index;
+	    	_event.color = color;
+	    	mHistory.Add(_event);
 	    }
 	  }
 	  
-	  public void gfxColor(int index, Color)
+	  public void gfxColor(int index, Color color)
 	  {
 	    if (index >= 0 && index < mData.Length)
 	    {
-	    
+	    	VisualArrayHistory _event;
+	    	_event.type = HISTORY_TYPE.COLOR;
+	    	_event.pos = index;
+	    	_event.color = color;
+	    	mHistory.Add(_event);
 	    }
 	  }
 	  
@@ -68,15 +98,23 @@ public class VisualArray
 	  {
 	    if (index >= 0 && index < mData.Length)
 	    {
-	    
+	    	VisualArrayHistory _event;
+	    	_event.type = HISTORY_TYPE.DECOLOR;
+	    	_event.pos = index;
+	    	mHistory.Add(_event);
 	    }
 	  }
 	  
-	  public void gfxSeparate(int leftindex, int size, Color)
+	  public void gfxSeparate(int leftindex, int size, Color color)
 	  {
 	    if (index >= -1 && index <= mData.Length)
 	    {
-	    
+	    	VisualArrayHistory _event;
+	    	_event.type = HISTORY_TYPE.SEPARATE;
+	    	_event.pos = leftindex;
+	    	_event.color = color;
+	    	_event.size = size;
+	    	mHistory.Add(_event);
 	    }
 	  }
 	  
@@ -84,18 +122,24 @@ public class VisualArray
 	  {
 	    if (index >= -1 && index <= mData.Length)
 	    {
-	    
+	    	VisualArrayHistory _event;
+	    	_event.type = HISTORY_TYPE.DESEPARATE;
+	    	_event.pos = leftindex;
+	    	mHistory.Add(_event);
 	    }
 	  }
 	  
 	  public void hideLastOperation()
 	  {
-	    
+	    if (mHistory.Count > 0) 
+	    {
+	    	mHistory.RemoveAt(mHistory.Count - 1); 	
+	    }
 	  }
 	  
 	  public void clearHistory()
 	  {
-	    mHistory.clear();
+	    mHistory.Clear();
 	  }
 	  
 	  public void render()
@@ -105,11 +149,20 @@ public class VisualArray
 	  
 	  private VisualArrayHistory nextOperation()
 	  {
-	  
+	  	VisualArrayHistory op = mHistory[mHistory.Count - 1];
+	  	
+	  	//TODO: Check swap
+	  	
+	  	return op;
 	  }
 	  
 	  private void renderNext()
 	  {
-	  
+	  	if (mHistory.Count > 0)
+	  	{
+	  		VisualArrayHistory op = nextOperation();
+	  		
+	  		//TODO ...
+	  	}
 	  }
 }
