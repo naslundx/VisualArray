0b86e859f5df7d0dd047446ca82d5f08192d5382
Marcus Näslund <naslundx@gmail.com>
2015-06-20 16:42:13 +0200

    Conform to new h file

--- cpp/visualarray.cpp
+++ cpp/visualarray.cpp
@@ -2,119 +2,125 @@
  *
  * So many things
  */
+ 
+//template<class T>
+VisualArrayData::VisualArrayData() {
+	parent = NULL;
+}
 
-template<class T>
+//template<class T>
+VisualArrayData::VisualArrayData(int _pos, VisualArray* _parent) {
+	pos = _pos;
+	parent = _parent;
+}
+
+//template<class T>
 VisualArray::VisualArray(int size) {
 	if (size>0) {
-		mData = new T[size];
-		mOriginal = new T[size];
-		mSize = 0;
+		mData = new VisualArrayData[size];
+		mOriginal = new VisualArrayData[size];
+		mSize = size;
+		
+		for (int i=0; i<size; i++) {
+			mData = VisualArrayData(i, this);
+			mOriginal = VisualArrayData(i, this);
+		}
 	}
 }
 
 //TODO: Allow to init with existing array and {...} as in C++11
 
-template<class T>
+//template<class T>
 VisualArray::~VisualArray() {
 	delete[] mData;
 	delete[] mOriginal;
 }
 
-template<class T>
-T VisualArray::operator[](int i) const {
+//template<class T>
+VisualArrayData VisualArray::operator[](int i) const {
 	return mData[i];
 }
 
-template<class T>
-T& VisualArray::operator [](const int i) {
-	// This will not allow me to notice change :(
-	// Figure out how to do this without specific set function
-
-	// Add change to mHistory:
-	std::vector<int> op;
-	op.push_back(HISTORY_TYPE.SET);
-
-	//...
-
-	mHistory.push_back(op);
+//template<class T>
+VisualArrayData& VisualArray::operator [](const int i) {
+	// Note: History is tracked from the VisualArrayData class
+	return &(mData[i]);
 }
 
-template<class T>
-void VisualArray::gfxHighlight(int index, Color) {
+//template<class T>
+void VisualArray::gfxHighlight(int index, Color color) {
 	if (index >= 0 && index < mSize) {
-		std::vector<int> op;  // std::vector<int> IS THE WRONG THING TO USE, USE THE VisualArrayData STRUCT!
-		op.push_back(HISTORY_TYPE.HIGHLIGHT);
-		op.push_back(index);
+		VisualArrayHistory event;
+		
+		event.type = VisualArray.HISTORY_TYPE.HIGHLIGHT;
+		event.pos = index;
+		event.color = color;
 
-		// handle color
-
-		mHistory.push_back(op);
+		mHistory.push_back(event);
 	}
 }
 
-template<class T>
+//template<class T>
 void VisualArray::gfxColor(int index, Color) {
 	if (index >= 0 && index < mSize) {
-		std::vector<int> op; // std::vector<int> IS THE WRONG THING TO USE, USE THE VisualArrayData STRUCT!
-		op.push_back(HISTORY_TYPE.COLOR);
-		op.push_back(index);
-
-		// handle color
+		VisualArrayHistory event;
+		
+		event.type = VisualArray.HISTORY_TYPE.COLOR;
+		event.pos = index;
+		event.color = color;
 
 		mHistory.push_back(op);
 	}
 }
 
-template<class T>
+//template<class T>
 void VisualArray::gfxDecolor(int index) {
 	if (index >= 0 && index < mSize) {
-		std::vector<int> op; // std::vector<int> IS THE WRONG THING TO USE, USE THE VisualArrayData STRUCT!
-		op.push_back(HISTORY_TYPE.DECOLOR);
-		op.push_back(index);
-
-		// handle color
+		VisualArrayHistory event;
+		
+		event.type = VisualArray.HISTORY_TYPE.DECOLOR;
+		event.pos = index;
 
 		mHistory.push_back(op);
 	}
 }
 
-template<class T>
-void VisualArray::gfxSeparate(int leftindex, int size, Color) {
+//template<class T>
+void VisualArray::gfxSeparate(int leftindex, float size, Color color) {
 	if (leftindex >= -1 && index <= mSize) {
-		std::vector<int> op; // std::vector<int> IS THE WRONG THING TO USE, USE THE VisualArrayData STRUCT!
-		op.push_back(HISTORY_TYPE.SEPARATE);
-		op.push_back(index);
-		op.push_back(size);
-
-		// handle color
+		VisualArrayHistory event;
+		
+		event.type = VisualArray.HISTORY_TYPE.SEPARATE;
+		event.pos = leftindex;
+		event.size = size;
+		event.color = color;
 
 		mHistory.push_back(op);
 	}
 
 }
 
-template<class T>
+//template<class T>
 void VisualArray::gfxDeseparate(int leftindex) {
 	if (leftindex >= -1 && index <= mSize) {
-		std::vector<int> op; // std::vector<int> IS THE WRONG THING TO USE, USE THE VisualArrayData STRUCT!
-		op.push_back(HISTORY_TYPE.DESEPARATE);
-		op.push_back(index);
-
-		// handle color
+		VisualArrayHistory event;
+		
+		event.type = VisualArray.HISTORY_TYPE.DESEPARATE;
+		event.pos = index;
 
 		mHistory.push_back(op);
 	}
 
 }
 
-template<class T>
+//template<class T>
 void VisualArray::hideLastOperation() {
 	if (!mHistory.empty()) {
 		mHistory.pop_back();
 	}
 }
 
-template<class T>
+//template<class T>
 void VisualArray::clearHistory() {
 	mHistory.clear();
 	for (int i=0; i<mSize; i++) {
@@ -123,7 +129,17 @@ void VisualArray::clearHistory() {
 	mGraphicsReady = true;
 }
 
-template<class T>
+//template<class T>
+void VisualArray::addSetEvent(int pos, int data) {
+	// Called by the VisualArrayData 
+	VisualArrayHistory event;
+	event.type = HISTORY_TYPE.SET;
+	event.pos = pos;
+	event.data = data;
+	mHistory.push_back(event);
+}
+
+//template<class T>
 void VisualArray::render() {
 	if (!mGraphicsReady) {
 		// There is no data in mOriginal!
@@ -140,18 +156,18 @@ void VisualArray::render() {
 	// ...
 }
 
-template<class T>
-std::vector<int> VisualArray::nextOperation() {
-	std::vector<int> op = mHistory.pop_front(); // std::vector<int> IS THE WRONG THING TO USE, USE THE VisualArrayData STRUCT!
-
+//template<class T>
+VisualArrayHistory VisualArray::nextOperation() {
+	VisualArrayHistory op = mHistory.pop_front(); 
+	
 	if (op[0] == HISTORY_TYPE.SET) {
 		if (!mHistory.empty()) {
-			std::vector<int> opb = mHistory.pop_front(); // std::vector<int> IS THE WRONG THING TO USE, USE THE VisualArrayData STRUCT!
+			VisualArrayHistory opb = mHistory.pop_front();
 
 			// Check if swap
-			if (opb[0] == HISTORY_TYPE.SET && op[2] == mOriginal[opb[1]] && opb[2] == mOriginal[op[1]]) {
-				op[0] = HISTORY_TYPE.SWAP;
-				op[2] = opb[1];
+			if (opb.event == HISTORY_TYPE.SET && op.data == mOriginal[opb.pos] && opb.data == mOriginal[op.pos]) {
+				op.event = HISTORY_TYPE.SWAP;
+				op.data = opb.pos;
 
 				// Swap elements
 				// ...
@@ -164,13 +180,13 @@ std::vector<int> VisualArray::nextOperation() {
 		}
 
 		// If we didn't return already, we should perform set
-		mOriginal[op[1]] = op[2];
+		mOriginal[op.pos] = op.data;
 	}
 
 	return op;
 }
 
-template<class T>
+//template<class T>
 void VisualArray::renderNext() {
 	std::vector<int>::iterator it; 
 	for (it = mLastHighlights.begin(); it != mLastHighlights.end(); ++it) {
@@ -178,7 +194,7 @@ void VisualArray::renderNext() {
 	}
 
 	if (!mHistory.empty()) {
-		std::vector<int> op = nextOperation(); // std::vector<int> IS THE WRONG THING TO USE, USE THE VisualArrayData STRUCT!
+		VisualArrayHistory op = nextOperation();
 
 		// Render this operation
 	}
